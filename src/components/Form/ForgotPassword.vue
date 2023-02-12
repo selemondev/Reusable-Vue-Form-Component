5e796059f2c34ff903ca68453318e60ad1dca7a5
selemondev <selemondev19@gmail.com>
2023-02-12 18:32:30 +0300

    add: router

---
+++ src/components/Form/ForgotPassword.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+    <div class="flex justify-end">
+        <p></p>
+    </div>
+</template>
---
+++ src/views/RegisterView.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+const handleClick = () => {
+    console.log("Clicked from App.vue")
+}
+</script>
+
+<template>
+     <RegisterForm @submit="handleClick()" type="Sign Up" typeDescription="Sign Up to get started"/>
+</template>
