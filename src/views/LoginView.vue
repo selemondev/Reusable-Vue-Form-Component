03b1dacd861902f7fd8800a2f6bada853abb60f3
selemondev <selemondev19@gmail.com>
2023-02-27 23:09:48 +0300

    add: supabase & title

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -2,13 +2,9 @@
 const handleClick = () => {
     console.log("Clicked from App.vue")
 };
-const formData = {
-    email: "",
-    password: ""
-};
 const route = "/register"
 </script>
 
 <template>
-     <LoginForm @submit="handleClick()" :to='`${route}`' :formData="formData" type="Sign In" typeDescription="Sign In below to get access to your account"/>
+     <LoginForm @submit="handleClick()" :to='`${route}`' type="Sign In" typeDescription="Sign In below to get access to your account"/>
 </template>
